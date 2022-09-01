b6f05124d2de860339a1ea2dccb9c6577b906282
Banzyworld <102680772+Banzyworld@users.noreply.github.com>
2022-09-01 01:42:21 +0100

    react-router: imported router props to route the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Login from './components/Login';
+import NavBar from './components/NavBar';
+import { SignUp } from './components/SignUp';
+import {Routes, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <NavBar />  
+      <Routes> 
+          
+        <Route path='/' element={<Login/>} />    
+        <Route path='/SignUp' element={<SignUp/>} />
+        
+           
+          
+        </Routes>
+
+
     </div>
   );
 }
